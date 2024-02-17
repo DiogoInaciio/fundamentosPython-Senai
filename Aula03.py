5882cad99bdd62296589d9b005b93a428dcc72de
Diogo <diogoremi1@gmail.com>
2024-02-17 15:05:56 -0300

    Aula 03

---
+++ Aula03.py
@@ -0,0 +1,24 @@
+#Exemplo
+a = 10
+b = 20
+print('A = ', a)
+print('B = ', b)
+if a > b:
+  print('A é maior que B!')
+
+else:
+  print('B é maior que A!')
+
+#Aquecimento para Estruturas Condicionais
+a = 40
+b = 8
+c = 50
+print('A = ', a)
+print('B = ', b)
+print('C = ', c)
+if a + b < c:
+  print('S, Será possível levar todos os alunos e monitores em apenas uma viagem! ')
+
+else:
+  print('N, Não será possível levar todos os alunos e monitores em apenas uma viagem!')
+
