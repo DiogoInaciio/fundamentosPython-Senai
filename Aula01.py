6b7ee65584dfbb8c6eb4b4e1f940c4a207fca3b0
Diogo <diogoremi1@gmail.com>
2024-02-17 14:44:23 -0300

    Aula 01

---
+++ Aula01.py
@@ -0,0 +1,13 @@
+idade = 20
+nome = 'Diogo'
+mes = 'Junho'
+
+#nome = input("Qual é o seu nome? ")
+#print("O meu nome é: ", nome)
+#print('Dia', 'Mes', 'Ano', sep= '/')
+
+a = 'A turma '
+b = 'Fundamentos de python '
+c = 'é top'
+
+print(a + b + c)
