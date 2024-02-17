ecfbe495adc6fd76c0ed5f03e8bbfb93ece5efbe
Diogo <diogoremi1@gmail.com>
2024-02-17 14:55:50 -0300

    Aula 02

---
+++ Aula02.py
@@ -0,0 +1,49 @@
+#Exemplo
+x = 10
+y = 20
+alfa = 30
+resul = x + y + alfa
+print('O resultado da soma é: ',resul)
+
+#Exemplo
+n = 12
+print("Resultado:", n**2)
+
+#Exemplo
+n1 = 25
+n2 = 5
+div = n1 / n2
+print('A divisão é ', div)
+
+#Exemplo
+import math
+n1 = 144
+print('A raiz quadrada é: ', math.sqrt(n1))
+
+#Exemplo
+a = 3
+b = -4
+soma = a+b
+sub = a-b
+mult = a*b
+div = a/b
+
+print('Soma: ', soma)
+print('Subtração: ', sub)
+print('Multiplicação: ', mult)
+print('Divisão: ', div)
+print('Cálculos dentro do print: ', (a*b))
+print('Resto da divisão de 11 por a: ', (11%a))
+print('a ==b: ', (a==b))
+print('a < 5: ', (a<5))
+
+logico1 = True
+logico2 = False
+
+print(type(logico1))
+print(type(logico2))
+print(logico1)
+print(not logico1)
+print(logico1 or logico2)
+print(logico1 and logico2)
+
